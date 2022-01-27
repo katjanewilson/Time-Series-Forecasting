2b749a39819ad93074d393db27ca1a7c09b37a8c
Kat Wilson <katjanewilson@gmail.com>
2022-01-27 21:10:19 +0000

    edts

--- assignment_1.R
+++ assignment_1.R
@@ -47,10 +47,39 @@ acf(ts(resid(model1)))
 
 
 ### question 3
+sales.ts <- ts(sales, freq=12)
+sales.decmps <- decompose(sales.ts)
+seasd <- sales.decmps$seasonal
+seasd[1:12]
+# compare to index estimates from model 1
+options(digits =5)
+cbind(seas, seasd[1:12])
+
 
 ### question 4
+model <- model1
+lresid<-c(rep(NA,360))
+lag1resid<-lresid
+lag2resid<-lresid
+lag3resid<-lresid
+lag1resid[2]<-resid(model)[1]
+lag1resid[3]<-resid(model)[2]
+lag2resid[3]<-resid(model)[1]
+for(i in 4:360){
+  i1<-i-1;i2<-i-2;i3<-i-3
+  lag1resid[i]<-resid(model)[i1];lag2resid[i]<-resid(model)[i2]
+  lag3resid[i]<-resid(model)[i3]
+}
+
+## now do residuals on this new model 
+qqnorm(resid(model))
+qqline(resid(model))
+plot(ts(resid(model), start = c(1992,1),
+        freq = 12), ylab = "Model 1 residuals")
 
 ### question 5
 
+model<-
+  lm(logSales~poly(Time,4)+fMonth+c348+s348+c432+s432,data=sales[1:192,])
 
 
